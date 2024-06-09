dd294f67c7e2ccb3fe8fb9f9227e8d1af3871e3d
Christopher Hamilton <christopher@39digits.com>
2024-06-09 19:50:14 +0100

    Getting back into Laravel

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,8 @@
+###########################
+## Node
 # List all globally installed npm modules without listing their dependencies
 alias npm-globals="npm list -g --depth=0"
 
-# alias start-mongo="brew services start mongodb/brew/mongodb-community"
-# alias stop-mongo="brew services stop mongodb/brew/mongodb-community"
+###########################
+## Laravel
+alias sail="./vendor/bin/sail"
